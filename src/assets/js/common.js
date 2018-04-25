8266b64f85d166f72fcaf940ce03644d10458668
Thomas <stithomas@deloitte.com>
2018-04-25 13:09:53 +0530

    added global js, less files and updated report 1099 page

---
+++ src/assets/js/common.js
@@ -0,0 +1,22 @@
+/*
+This file contains common function and js code 
+used accross the application
+*/
+
+var triggerAjaxRequest = function(data,type,url){   
+    function successCallback(res){
+        return res;
+    }
+    function errorCallback(err){
+        return err;
+    }
+    return $.ajax({
+        type: type,
+        data: data,
+        contentType: "application/json",
+        dataType:"json",
+        url: url,
+        success: successCallback,
+        error: errorCallback
+    });
+};
