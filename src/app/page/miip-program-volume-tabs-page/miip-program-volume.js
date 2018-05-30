0f4ff20fee7effba2ee4085323ae32fc0cedaa9c
Thomas <stithomas@deloitte.com>
2018-05-30 13:38:00 +0530

    volume view dispute and sales modal added

--- src/app/page/miip-program-volume-tabs-page/miip-program-volume.js
+++ src/app/page/miip-program-volume-tabs-page/miip-program-volume.js
@@ -10,8 +10,11 @@ require(["modernizr",
     "text!app/page/miip-program-volume-tabs-page/programView.hbs",
     "text!app/page/miip-program-volume-tabs-page/volumeView.hbs",
     "text!app/page/miip-program-volume-tabs-page/programVolumeHeading.hbs",
+    "text!app/page/miip-program-volume-tabs-page/salesModal.hbs",
+    "text!app/page/miip-program-volume-tabs-page/disputeModal.hbs",
+    "text!app/page/miip-program-volume-tabs-page/disputedModal.hbs",
 
-], function(modernizr, $, bootstrap, Handlebars, bootstrapSelect, bootstrapTable, _defaultDdnHBS, _programViewSummaryHBS, _programVolumeDetailsHBS, _programViewHBS, _volumeViewHBS, _programVolumeHeadingHBS) {
+], function(modernizr, $, bootstrap, Handlebars, bootstrapSelect, bootstrapTable, _defaultDdnHBS, _programViewSummaryHBS, _programVolumeDetailsHBS, _programViewHBS, _volumeViewHBS, _programVolumeHeadingHBS, _salesModalHBS, _disputeModalHBS, _disputedModalHBS) {
 
     //Compiling HBS templates
     var compiledDefaultDdn = Handlebars.compile(_defaultDdnHBS);
@@ -20,6 +23,9 @@ require(["modernizr",
     var compiledProgramView = Handlebars.compile(_programViewHBS);
     var compiledVolumeView = Handlebars.compile(_volumeViewHBS);
     var compiledProgramVolumeHeading = Handlebars.compile(_programVolumeHeadingHBS);
+    var compiledSalesModal = Handlebars.compile(_salesModalHBS);
+    var compiledDisputeModal = Handlebars.compile(_disputeModalHBS);
+    var compiledDisputedModal = Handlebars.compile(_disputedModalHBS);
 
     var miipProgramVolumePage = (function() {
 
@@ -35,7 +41,12 @@ require(["modernizr",
             programViewContainer: ".js-program-view",
             volumeViewContainer: ".js-volume-view",
             programVolumeHeadingContainer: ".js-program-volume-heading",
-            sortByDdnContainer: ".js-sortbyDdn"
+            sortByDdnContainer: ".js-sortbyDdn",
+            selectedTabs: '.content-tabs .nav-tabs a[data-toggle="tab"]',
+            selectedDisputeLink: 'a[data-target="#disputeModal"]',
+            salesModal: ".js-sales-modal",
+            disputeModal: ".js-dispute-modal",
+            disputedModal: ".js-disputed-modal"
         };
 
         var srtByDdn = {
@@ -89,6 +100,9 @@ require(["modernizr",
             $(config.volumeViewContainer).html(compiledVolumeView());
             $(config.programVolumeHeadingContainer).html(compiledProgramVolumeHeading(cbp.miipProgramVolumeDetailPage));
             $(config.sortByDdnContainer).html(compiledDefaultDdn(srtByDdn));
+            $(config.salesModal).html(compiledSalesModal());
+            $(config.disputeModal).html(compiledDisputeModal());
+            $(config.disputedModal).html(compiledDisputedModal());
             $(config.dropDownCommon).selectpicker('refresh');
             enableMobileDefaultDropDown();
         };
@@ -99,6 +113,18 @@ require(["modernizr",
                 $(config.dropDownCommon).selectpicker('mobile');
             }
         };
+
+        var initalizingTables = function(){
+            $('#disputeTable, #disputedTable, #addSalesTable').bootstrapTable({
+                classes: 'table table-no-bordered',
+                striped: true,
+                iconsPrefix: 'fa',
+                responsive: true,
+                responsiveBreakPoint: 768,
+                responsiveClass: "bootstrap-table-cardview",
+                undefinedText: ""
+            });
+        }
 
         var populateTable = function(){
             $('#programTable').bootstrapTable({
@@ -133,7 +159,7 @@ require(["modernizr",
                     title: 'Amortization End Date'
                 }, {
                     field: 'totalPaid',
-                    title: 'Total Paid',
+                    title: 'Total Paid (USD)',
                     class: 'text-right'
                 },
                 {
@@ -196,17 +222,24 @@ require(["modernizr",
                     field: 'disputeVolume',
                     title: 'Dispute Volume',
                     formatter: function(row, value, index){
+                        console.log(value);
                         if($.inArray( value, volumeRowArray) < 0){
                             volumeRowArray.splice(index, 0, value);
                         }
-                        return '<a href="" data-toggle="modal" data-target="#disputeModal" data-index='+index+'>'+row+'</a>';
+
+                        if(value.disputeVolume === 'Disputed'){
+                            return '<a href="" data-toggle="modal" data-target="#disputedModal" data-index='+index+'>'+row+'</a>';
+                        } else{
+                            return '<a href="" data-toggle="modal" data-target="#disputeModal" data-index='+index+'>'+row+'</a>';
+                        }
                     }
                 }, {
                     field: 'reason',
                     title: 'Reason'
                 }, {
                     field: 'status',
-                    title: 'Status'
+                    title: 'Status',
+                    sortable: true
                 }],
                 data: [{
                     salesMonth: 'Sept 2017',
@@ -232,7 +265,7 @@ require(["modernizr",
                     mul: '90,123',
                     pul: '69,279',
                     total: '192,105',
-                    disputeVolume: 'Dispute',
+                    disputeVolume: 'Disputed',
                     reason: '',
                     status: ''
                 }]
@@ -240,7 +273,7 @@ require(["modernizr",
         }
 
         var bindEvents = function(){
-            $(document).on('shown.bs.tab','a[data-toggle="tab"]', function (e) {
+            $(document).on('shown.bs.tab',config.selectedTabs, function (e) {
                 var target = $(e.target).attr("href");
                 if(target === '#programview'){
                     cbp.miipProgramVolumeDetailPage.programView = true;
@@ -251,13 +284,14 @@ require(["modernizr",
                 $(config.programViewSummaryConatiner).html(compiledProgramViewSummary(cbp.miipProgramVolumeDetailPage));
             });
 
-            $(document).on('click','a[data-target="#disputeModal"]', function(e){
-                console.log(volumeRowArray);
+            $(document).on('click',config.selectedDisputeLink, function(e){
+                var targetDataIndex = e.target.dataset.index;
             });
         }
 
         var init = function() {
             loadingInitialHbsTemplates();
+            initalizingTables();
             populateTable();
             bindEvents();
         };
